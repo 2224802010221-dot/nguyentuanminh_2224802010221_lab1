0eed264aa64078aea077c18d0e783e63f85643b3
Minhzzzs <nguyentuanminh977816@gmail.com>
2026-04-01 18:21:05 +0700

    Lab 1.2 - Dart program

---
+++ test/nguyentuanminh_2224802010221_lab1_2_test.dart
@@ -0,0 +1,8 @@
+import 'package:nguyentuanminh_2224802010221_lab1_2/nguyentuanminh_2224802010221_lab1_2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
